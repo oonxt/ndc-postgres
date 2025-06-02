12c180e159ee5f6ce525f2234b3c86e010004816
oonxt <oonxt@msn.com>
2025-06-02 11:56:42 +0800

    Update lib.rs

--- crates/connectors/ndc-postgres/src/lib.rs
+++ crates/connectors/ndc-postgres/src/lib.rs
@@ -12,3 +12,9 @@ pub mod state;
 
 // we expose the sdk used for this connector so that we are able to use it in multitenant
 pub use ndc_sdk as sdk;
+
+pub use ndc_postgres_configuration;
+pub use query_engine_execution;
+pub use query_engine_metadata;
+pub use query_engine_sql;
+pub use query_engine_translation;
